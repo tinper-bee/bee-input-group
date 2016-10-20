35d32397346283d7915c45a51f2cbf2485dfd6f0
ahua52 <1468492018@qq.com>
2016-10-20 10:47:06 +0800

    ver. 0.0.2

---
+++ build/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _InputGroup = require('./InputGroup');
+
+var _InputGroup2 = _interopRequireDefault(_InputGroup);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+exports["default"] = _InputGroup2["default"];
+module.exports = exports['default'];
