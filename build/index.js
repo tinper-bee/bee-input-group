137f0bec2f77240d952e0b8b1d8d48e2beef6908
“liurhc” <liurhc@yonyou.com>
2016-11-03 14:41:16 +0800

    ver. 0.0.5

--- build/index.js
+++ build/index.js
@@ -1,18 +1,3 @@
 'use strict';
 
-<<<<<<< HEAD
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _InputGroup = require('./InputGroup');
-
-var _InputGroup2 = _interopRequireDefault(_InputGroup);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-exports["default"] = _InputGroup2["default"];
-module.exports = exports['default'];
-=======
 module.exports = require('./InputGroup');
->>>>>>> c011692d7da1bfeccd2cce5c7f8064b8b593f641
